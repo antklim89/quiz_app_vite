819b7afd483c2f60ab6d87d9afd1d39900fd38f5
Anton <antklim89@mail.ru>
2021-09-27 21:17:43 +0300

    fix boolean

--- src/types/question.ts
+++ src/types/question.ts
@@ -10,14 +10,14 @@ export interface QuestionResponse {
       answer_e: string
       answer_f: string
     }
-    multiple_correct_answers: boolean
+    multiple_correct_answers: string
     correct_answers: {
-      answer_a_correct: boolean
-      answer_b_correct: boolean
-      answer_c_correct: boolean
-      answer_d_correct: boolean
-      answer_e_correct: boolean
-      answer_f_correct: boolean
+      answer_a_correct: string
+      answer_b_correct: string
+      answer_c_correct: string
+      answer_d_correct: string
+      answer_e_correct: string
+      answer_f_correct: string
     }
     explanation: string
     tip: string|null
