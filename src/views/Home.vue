d1f7390840e50f4f69a5e01f6d37dc04fefb6df1
Anton <antklim89@mail.ru>
2023-04-30 21:02:51 +0300

    update categories styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,14 +4,8 @@
             <div
                 v-for="category in CATEGORIES"
                 :key="category"
-                class="category select"
+                class="category"
             >
-                <label
-                    :for="category"
-                    :class="{checked: store.state.selectedCategory === category}"
-                >
-                    {{ category }}
-                </label>
                 <input
                     :id="category"
                     :checked="store.state.selectedCategory === category"
@@ -19,19 +13,15 @@
                     name="category"
                     @input="handleSelectCategory(category)"
                 >
+                <label
+                    :for="category"
+                    :class="{checked: store.state.selectedCategory === category}"
+                >
+                    {{ category }}
+                </label>
             </div>
         </div>
         <div class="button-group">
-            <div>
-                <button
-                    class="button"
-                    :class="{loading: loading}"
-                    :disabled="loading"
-                    @click="startQuiz"
-                >
-                    Start New
-                </button>
-            </div>
             <div>
                 <router-link
                     v-if="store.getters.hasQuestions"
@@ -42,6 +32,16 @@
                 >
                     Continue
                 </router-link>
+            </div>
+            <div>
+                <button
+                    class="button"
+                    :class="{loading: loading}"
+                    :disabled="loading"
+                    @click="startQuiz"
+                >
+                    Start New
+                </button>
             </div>
             <div>
                 <router-link
@@ -94,14 +94,48 @@ const handleSelectCategory = (value: string) => {
 <style lang="scss" scoped>
 .categories {
     display: grid;
-    grid-template-columns: repeat(var(--cols), 1fr);
+    grid-template-columns: repeat(1, 1fr);
     gap: var(--sp-2);
+
+    @media screen and (min-width: 480px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media screen and (min-width: 1080px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
 }
 
 .category {
+    padding: var(--sp-2);
+    display: flex;
+
     label {
+        --outline-width: 2px;
         font-size: var(--fs-lg);
         text-transform: uppercase;
+        width: 100%;
+        box-shadow: 0 0 0px var(--outline-width) var(--primary-color);
+        padding: var(--sp-2);
+        cursor: pointer;
+        text-align: center;
+        transition: 100ms;
+        border-radius: 9999px;
+    }
+
+    input:focus + label  {
+        outline: 2px solid var(--focus-color);
+        outline-offset: -2px;
+    }
+
+    input:checked + label {
+        --outline-width: 8px;
+    }
+
+    input {
+        outline: none;
+        width: 0;
+        height: 0;
     }
 }
 </style>
