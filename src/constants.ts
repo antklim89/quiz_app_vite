c62d13e705c39a3b66a181c9760a016bcf9b4a5c
Anton <antklim89@mail.ru>
2023-04-30 20:35:13 +0300

    remove uncategorized and devops categories

--- src/constants.ts
+++ src/constants.ts
@@ -2,12 +2,10 @@ export const URL = '/.netlify/functions/questions';
 export const QUESTIONS_STORAGE_NAME = 'QUIZ_APP_QUESTIONS';
 
 export const CATEGORIES = [
-    'uncategorized',
     'linux',
     'bash',
     'docker',
     'sql',
     'cms',
     'code',
-    'devops',
 ];
